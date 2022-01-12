a271d2963c13f63f6f7e2d118dd2543e19539a4d
osama <osama.w.hafez@gmail.com>
2022-01-11 19:32:08 -0500

    no tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -4,16 +4,16 @@ const lessonTests = require("./lesson-tests.js").lessonTests;
 const setup = require("./setup.js");
 const utils = require("./utils.js");
 
-describe("Set up", () => {
-    test("setup database", async () => {
-        let db = await utils.connect_to_db();
-        await setup.seedDatabase(db);
-    }, 30000);
-});
+// describe("Set up", () => {
+//     test("setup database", async () => {
+//         let db = await utils.connect_to_db();
+//         await setup.seedDatabase(db);
+//     }, 30000);
+// });
 
-describe("Test Lesson", () => {
-    lessonTests();
-});
+// describe("Test Lesson", () => {
+//     lessonTests();
+// });
 
 // describe("Test Registration", () => {
 //     registrationTests();
